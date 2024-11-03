f4fc7c739c4032541a1f825a802e8f5054fc0bb1
Nothingsware <94577071+nothingsware@users.noreply.github.com>
2024-11-03 18:35:12 -0500

    Create catv5.lua

---
+++ catv5.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/refs/heads/main/Libraries/Installer.lua"))()
+
+--and this 2nd
+getgenv().CAK = "CAK_4WYRSBC6M-DMcSGO-BZTBWU5pV48ui6g"
+loadfile("catvape/loader.lua")()
